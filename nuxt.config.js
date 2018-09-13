ca5caea401825fd0ddf74a27ea5ff04799cddbde
Seongjae Hwang <sjhwang@syncodax.com>
2018-09-13 17:19:44 +0900

    genuine initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'pre-view',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Nuxt.js project'},
+      {keywords: 'Vue.js'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: {color: '#3B8070'},
+  /*
+  ** Build configuration
+  */
+  build: {
+    extend(config, {isDev, isClient}) {
+      config.resolve.extensions.push('.vue');
+
+      /*
+      ** Run ESLint on save
+      */
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+};
+
