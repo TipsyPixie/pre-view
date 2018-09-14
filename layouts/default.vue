0287807181440c85f369ad203f9d165a1f9a3e3a
Seongjae Hwang <sjhwang@syncodax.com>
2018-09-14 17:39:18 +0900

    sass support

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,52 +4,42 @@
   </div>
 </template>
 
-<style>
-  @import url('https://fonts.googleapis.com/css?family=Noto+Sans');
+<style lang="sass">
+  @import url('https://fonts.googleapis.com/css?family=Noto+Sans')
 
-  html {
-    font-family: "Noto Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-    font-size: 16px;
-    word-spacing: 1px;
-    -ms-text-size-adjust: 100%;
-    -webkit-text-size-adjust: 100%;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    box-sizing: border-box;
-  }
+  html
+    font-family: "Noto Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif
+    font-size: 16px
+    word-spacing: 1px
+    -ms-text-size-adjust: 100%
+    -webkit-text-size-adjust: 100%
+    -moz-osx-font-smoothing: grayscale
+    -webkit-font-smoothing: antialiased
+    box-sizing: border-box
 
-  *, *:before, *:after {
-    box-sizing: border-box;
-    margin: 0;
-  }
+  *
+    &:before, &:after
+      box-sizing: border-box
+      margin: 0
 
-  .button--green {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #3b8070;
-    color: #3b8070;
-    text-decoration: none;
-    padding: 10px 30px;
-  }
+  @mixin button($color, $border-color, $hover-background-color)
+    $white: #fff
+    display: inline-block
+    border-radius: 4px
+    border: 1px solid $border-color
+    color: $color
+    text-decoration: none
+    padding: 10px 30px
+    &:hover
+      color: $white
+      background-color: $hover-background-color
 
-  .button--green:hover {
-    color: #fff;
-    background-color: #3b8070;
-  }
+  .button--green
+    $green: #3b8070
+    @include button($green, $green, $green)
 
-  .button--grey {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #35495e;
-    color: #35495e;
-    text-decoration: none;
-    padding: 10px 30px;
-    margin-left: 15px;
-  }
-
-  .button--grey:hover {
-    color: #fff;
-    background-color: #35495e;
-  }
+  .button--grey
+    $grey: #35495e
+    @include button($grey, $grey, $grey)
 </style>
 
